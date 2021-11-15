b743ff6446e19d59330063ceec4d5042904405c8
awallbark <awallbark@gmail.com>
2021-11-14 20:58:21 -0600

    Creates new Student

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,12 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            Student Toni = new Student();
+
+            Toni.Name = "Toni";
+            Toni.StudentId = 1;
+            Toni.NumberOfCredits = 1;
+            Toni.Gpa = 4.0;
         }
     }
 }
