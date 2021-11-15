e2c284591a5e9744b50453ac856b8efb1bf1898e
awallbark <awallbark@gmail.com>
2021-11-14 20:58:06 -0600

    Adds Course class

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    class Course
+    {
+        public int CourseID { get; set; }
+        public string CourseName { get; set; }
+        public string Instructor { get; set; }
+        public int Credits { get; set; }
+        public List<Student> Id {get; set;}
+
+
+    }
+}
