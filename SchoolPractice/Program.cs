df405527728742750e63a1ba9af8aa522f87179d
awallbark <awallbark@gmail.com>
2021-11-18 13:29:42 -0600

    Adds some code to test changes made to Student.cs

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,12 +7,13 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
-            Student Toni = new Student();
+            Student Toni = new Student("Toni", 1, 100, 4.0);
 
-            Toni.Name = "Toni";
-            Toni.StudentId = 1;
-            Toni.NumberOfCredits = 1;
-            Toni.Gpa = 4.0;
+            Console.WriteLine(Toni.GetGradeLevel());
+
+            Toni.NumberOfCredits = 40;
+
+            Console.WriteLine(Toni.GetGradeLevel());
         }
     }
 }
