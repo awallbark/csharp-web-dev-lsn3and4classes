b831203a27d90681db44661c0e0d494bf69e62d8
awallbark <awallbark@gmail.com>
2021-11-18 13:30:38 -0600

    Adds logic to GetGradeLevel method

--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -7,10 +7,65 @@ namespace SchoolPractice
 {
     public class Student
     {
+        private static int nextStudentId = 1;
         public string Name { get; set; }
-        public int StudentId { get; set; }
+        public readonly int studentId;
         public int NumberOfCredits { get; set; }
         public double Gpa { get; set; }
+
+        public Student(string name, int sId, int numberOfCredits, double gpa)
+        {
+            Name = name;
+            studentId = sId;
+            NumberOfCredits = numberOfCredits;
+            Gpa = gpa;
+        }
+
+        public Student(string name, int sId)
+        {
+            Name = name;
+            studentId = sId;
+            NumberOfCredits = 0;
+            Gpa = 0.0;
+        }
+
+        public Student(string name)
+        {
+            Name = name;
+            studentId = nextStudentId;
+            nextStudentId++;
+            NumberOfCredits = 0;
+            Gpa = 0.0;
+        }
+
+        public string StudentInfo()
+        {
+            return (Name + " has a GPA of: " + Gpa);
+        }
+
+        public void AddGrade(int courseCredits, double grade)
+        {
+            // Update the appropriate fields: NumberOfCredits, Gpa
+        }
+
+        public string GetGradeLevel()
+        {
+            // Determine the grade level of the student based on NumberOfCredits
+            if (NumberOfCredits <= 29)
+            {
+                return "Freshman";
+            }
+            else if (NumberOfCredits == 30 || NumberOfCredits <= 59)
+            {
+                return "Sophmore";
+            }
+            else if (NumberOfCredits == 60 || NumberOfCredits <= 89)
+            {
+                return "Junior";
+            }
+            else return "Senior";
+        }
+
 
     }
 
