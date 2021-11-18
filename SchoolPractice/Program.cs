05f15b2a9a78cdb331edd0ffff45625651539c6f
awallbark <awallbark@gmail.com>
2021-11-18 14:34:20 -0600

    Adds code to test Equal Method

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -8,11 +8,14 @@ namespace SchoolPractice
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
             Student Toni = new Student("Toni", 1, 100, 4.0);
+            Student Aaron = new Student("Aaron", 5, 4, 4.0);
 
             Console.WriteLine(Toni.GetGradeLevel());
 
             Toni.AddGrade(4, 4.0);
             Toni.AddGrade(4, 3.0);
+
+            Console.WriteLine(Toni.Equals(Aaron)); 
 
         }
     }
